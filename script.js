0d31d2357a48affda467d7b426971d5ed37f7e45
shouta40455-pixel <shouta40455@gmail.com>
2026-06-02 17:06:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+// ページ内にある「slider-container」をすべて見つける
 const containers = document.querySelectorAll('.slider-container');
 
+// 見つかったすべてのグループに対して、一つずつ処理を設定する
 containers.forEach((container) => {
+  // そのグループの中にあるパーツだけをピンポイントで取得
   const slider = container.querySelector('.slider');
   const afterWrap = container.querySelector('.image-after-wrap');
   const sliderButton = container.querySelector('.slider-button');
 
+  // そのスライダーが動かされたときの処理
   slider.addEventListener('input', (e) => {
     const value = e.target.value;
+    
+    // そのグループの画像と線だけを動かす
     afterWrap.style.width = `${value}%`;
     sliderButton.style.left = `${value}%`;
   });
